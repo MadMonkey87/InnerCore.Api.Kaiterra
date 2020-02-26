764ea1dfe249857ff9776b8434f5ef82fce45458
MadMonkey87 <myself@philippewechsler.ch>
2020-02-26 20:46:26 +0100

    sample applications

---
+++ InnerCore.Api.Kaiterra.BasicSample/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading.Tasks;
+using InnerCore.Api.Kaiterra.Models.Basic;
+
+namespace InnerCore.Api.Kaiterra.BasicSample
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("please enter your api key");
+            var apiKey = Console.ReadLine();
+
+            var client = new KaiterraBasicClient(apiKey);
+
+            PlotSensorReading(await client.GetLaserEggDetails("00000000-0001-0001-0000-00007e57c0de"));
+
+            PlotSensorReading(await client.GetSenseEdgeDetails("00000000-0031-0001-0000-00007e57c0de"));
+
+            Console.WriteLine("Press enter to quit");
+            Console.ReadLine();
+        }
+
+        private static void PlotSensorReading(SensorReading sensorReading)
+        {
+            Console.WriteLine($"deviceId: {sensorReading.DeviceId}");
+            Console.WriteLine($"   type: {sensorReading.DeviceType}");
+            Console.WriteLine($"   time stamp: {sensorReading.TimeStamp}");
+
+            if (sensorReading.CO2.HasValue)
+            {
+                Console.WriteLine($"   CO2: {sensorReading.CO2} ppm");
+            }
+            else
+            {
+                Console.WriteLine("   CO2: -");
+            }
+
+
+            if (sensorReading.Pm10.HasValue)
+            {
+                Console.WriteLine($"   Pm10: {sensorReading.Pm10} µg/m³");
+            }
+            else
+            {
+                Console.WriteLine("   Pm10: -");
+            }
+
+            if (sensorReading.Pm25.HasValue)
+            {
+                Console.WriteLine($"   Pm2.5: {sensorReading.Pm25} µg/m³");
+            }
+            else
+            {
+                Console.WriteLine("   Pm2.5: -");
+            }
+
+            if (sensorReading.TotalVolatileOrganicCompounds.HasValue)
+            {
+                Console.WriteLine($"   tVOC: {sensorReading.TotalVolatileOrganicCompounds} ppb");
+            }
+            else
+            {
+                Console.WriteLine("   tVOC: -");
+            }
+
+            if (sensorReading.RelativeHumidity.HasValue)
+            {
+                Console.WriteLine($"   relative humidity: {sensorReading.RelativeHumidity} %");
+            }
+            else
+            {
+                Console.WriteLine("   relative humidity: -");
+            }
+
+            if (sensorReading.Temperature.HasValue)
+            {
+                Console.WriteLine($"   temperature: {sensorReading.Temperature} C°");
+            }
+            else
+            {
+                Console.WriteLine("   temperature: -");
+            }
+        }
+    }
+}
---
+++ InnerCore.Api.Kaiterra.ExtendedSample/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using InnerCore.Api.Kaiterra.Models.Extended;
+
+namespace InnerCore.Api.Kaiterra.ExtendedSample
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("please enter your email address of your kaiterra account");
+            var email = Console.ReadLine();
+            Console.WriteLine("please enter your password");
+            var password = Console.ReadLine();
+
+            var client = new KaiterraExtendedClient();
+            await client.Login(email, password);
+
+            var devices = await client.GetDevices();
+
+            Console.WriteLine($"found {devices.Count()} device(s)");
+
+            if (!devices.Any())
+            {
+                Console.WriteLine("Press enter to quit");
+                Console.ReadLine();
+                return;
+            }
+
+            foreach (var device in devices)
+            {
+                Console.WriteLine($"    {device.Name}");
+            }
+
+            Console.WriteLine($"current values for '{devices.First().Name}'");
+            var history = await client.GetCurrentValues(devices.First().Id);
+            PlotValues(history);
+
+            history = await client.GetHistoricValues(devices.First().Id, DateTimeOffset.Now.AddDays(-30),
+                DateTimeOffset.Now, TimeSpan.FromMinutes(15));
+            Console.WriteLine($"there have been {history.Data.Length} recorded values for the last 30 days (15 minutes interval)");
+
+            Console.WriteLine("Press enter to quit");
+            Console.ReadLine();
+        }
+
+        private static void PlotValues(History history)
+        {
+            var data = history.Data.First();
+
+            Console.WriteLine($"   time stamp: {data.TimeStamp}");
+
+            if (data.Pollutants.AirQualityIndex.Value.HasValue)
+            {
+                Console.WriteLine($"   Air Quality Index: {data.Pollutants.AirQualityIndex.Value}");
+            }
+
+            if (data.Pollutants.CO2.Value.HasValue)
+            {
+                Console.WriteLine($"   CO2: {data.Pollutants.CO2.Value} {history.Units.CO2}");
+            }
+
+            if (data.Pollutants.Pm25.Value.HasValue)
+            {
+                Console.WriteLine($"   Pm2.5: {data.Pollutants.Pm25.Value} {history.Units.Pm25}");
+            }
+
+            if (data.Pollutants.Pm10.Value.HasValue)
+            {
+                Console.WriteLine($"   Pm10: {data.Pollutants.Pm10.Value} {history.Units.Pm10}");
+            }
+
+            if (data.Pollutants.TotalVolatileOrganicCompounds.Value.HasValue)
+            {
+                Console.WriteLine($"   tVOC: {data.Pollutants.TotalVolatileOrganicCompounds.Value} {history.Units.TotalVolatileOrganicCompounds}");
+            }
+        }
+    }
+}
